eb7b2c5fd0b22928188888d952dcb9e51ae7ba42
marcos-gc7 <marcos.yk07@gmail.com>
2025-05-13 16:20:31 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const sidebarItems = document.querySelectorAll('.sidebar .item')
 const tableRows = document.querySelectorAll('.main table tbody tr')
 
+const menuBtn = document.getElementById('menu-btn')
+const leftSection = document.querySelector('.left-section')
+
+let isMenuOpen = false
+
 sidebarItems.forEach(sideItem => {
-    sideItem.addEventListener('click', () =>{
-        sidebarItems.forEach(item =>{
+    sideItem.addEventListener('click', () => {
+        sidebarItems.forEach(item => {
             item.classList.remove('active')
         })
         sideItem.classList.add('active')
@@ -17,4 +22,13 @@ tableRows.forEach(tableTr => {
         })
         tableTr.classList.add('selected')
     })
+})
+
+menuBtn.addEventListener('click', () => {
+    if (!isMenuOpen) {
+        leftSection.style.left = '0'
+    } else {
+        leftSection.style.left = '-160px'
+    }
+    isMenuOpen = !isMenuOpen
 })
